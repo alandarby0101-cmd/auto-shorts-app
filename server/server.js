a9c7f4ce394d3c589cd3b419cce03ff22e54472e
Aland <alandarby0101@gmail.com>
2026-02-22 17:48:08 +0000

    fix static public path

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-app.use(express.static(path.resolve("public")));
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(session({
   secret: "autoshorst_secret_key",
   resave: false,
